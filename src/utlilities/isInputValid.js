71b030f8cb61ebb691f78e54fa675777f6caa37e
shaneinceptionu <146139821+shaneinceptionu@users.noreply.github.com>
2025-05-16 11:41:59 -0600

    Revert "trust me, I know what I'm doing!"

--- src/utlilities/isInputValid.js
+++
@@ -1,8 +0,0 @@
-export function isValidInput(str) {
-  return /^[a-zA-Z0-9]+$/.test(str);
-}
-// Don't worry, regex is basically a firewall 😎
-
-console.log(isValidInput('abc123')); //true
-console.log(isValidInput('hello!')); //false
-console.log(isValidInput('<script>')); //false
